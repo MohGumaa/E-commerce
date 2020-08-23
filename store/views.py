f066ee88801094584df17f221d627218b2f643bc
MohGumaa <gomaam14@gmail.com>
2020-08-23 14:11:45 +0530

    Add product, order and customer models

--- store/views.py
+++ store/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render
+from .models import *
 
 
 def store(request):
+    products = Product.objects.all()
     context = {
-        'title': 'Store'
+        'title': 'Store',
+        'products': products
     }
     return render(request, 'store/store.html', context)
 
 
 def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        print(order, created)
+        items = order.orderitem_set.all()
+    else :
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total':0}
     context = {
-        'title': 'Cart'
+        'title': 'Cart',
+        'items': items,
+        'order': order
     }
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        print(order, created)
+        items = order.orderitem_set.all()
+    else :
+        items = []
+        order = {'get_cart_items': 0, 'get_cart_total':0}
     context = {
-        'title': 'Checkout'
+        'title': 'Checkout',
+        'items': items,
+        'order': order
     }
     return render(request, 'store/checkout.html', context)
